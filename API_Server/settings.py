b6a965066cbcef9ac6d6b4846c1f6cbb34fd717a
ely28876 <funtag@naver.com>
2020-04-21 23:51:30 +0900

    fix: fix config_secret file root, add wsgi code

--- API_Server/settings.py
+++ API_Server/settings.py
@@ -14,7 +14,7 @@ import os
 import json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.dirname(BASE_DIR)
 
 # .config_secret directory roots
@@ -132,3 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'pubstatic')
--- API_Server/wsgi.py
+++ API_Server/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'API_Server.settings')
+# sys.path.append('C:/Apache24/htdocs')
 
 application = get_wsgi_application()
