a1fba133eacb002872d3d1e3719a8421591e13ac
ely28876 <funtag@naver.com>
2020-05-18 22:24:26 +0900

    feat: Connected MariaDB

--- API_Server/settings.py
+++ API_Server/settings.py
@@ -90,12 +90,9 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-"""DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}"""
+DATABASES = {
+    'default': config_secret_common['django']['database']
+}
 
 
 # Password validation
